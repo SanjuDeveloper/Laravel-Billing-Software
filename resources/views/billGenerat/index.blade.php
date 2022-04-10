fb44b8f3018eabc9fdfaa5ba9c5c2017f31d0d2e
SanjuDeveloper <bhattsanju.it@gmail.com>
2022-04-10 03:15:32 -0400

    Header-and-billGenerate-pagepe-created

--- resources/views/billGenerat/index.blade.php
+++ resources/views/billGenerat/index.blade.php
@@ -1,12 +1,28 @@
-@extends('header')
-@section('Section') 
+@include('layouts/head')
+
+        <!-- Sidebar -->
+        @include('layouts/sidebar')
+      
+        <!-- End of Sidebar -->
+          <!-- Content Wrapper -->
+          <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                @include('layouts/topbar')
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+
 
 <style>
     .bill{
      border:1px solid blue;
    }
      input{
-	   width:100% !important;
+	   width:50% !important;
 	 }
 	 #rep{
 	   background-color:grey;
@@ -37,172 +53,142 @@
 		border : 1px solid black;
 	  }
 	  
-.autocomplete-items {
-  position: absolute;
-  border: 1px solid #d4d4d4;
-  border-bottom: none;
-  border-top: none;
-  z-index: 99;
-  /*position the autocomplete items to be the same width as the container:*/
-  top: 100%;
-  left: 0;
-  right: 0;
-}
 
-.autocomplete-items div {
-  padding: 10px;
-  cursor: pointer;
-  background-color: #fff; 
-  border-bottom: 1px solid #d4d4d4; 
-}
-
-/*when hovering an item:*/
-.autocomplete-items div:hover {
-  background-color: #e9e9e9; 
-}
-
-/*when navigating through the items using the arrow keys:*/
-.autocomplete-active {
-  background-color: DodgerBlue !important; 
-  color: #ffffff; 
-}
-.autocomplete {
-  position: relative;
-  display: inline-block;
-}
 </style>
-		
+        <div class="card-header py-3">
+           <h3 class="text-uppercase" style="text-shadow: 1px 1px 2px blue,0 0 0.2em darkblue;color: black;font-size: 30px;margin-left:40%;">Generate Bill</h3>
+        </div>
 		<div class="container-fluid" style="padding:0 3%;overflow-y:auto;">
-			    <div class="bill p-3" style="background-color:lightgrey;overflow:auto">
-				    <form id="myform" >
-					    <table style="font-size:14px;width:100%;">
-						    <tbody>
-							    <tr>
-							        <td width="20%"><b>Bill No</b></td>
-									<td width="30%"><input name="bill_no" value="<?php echo(rand(10,100)); ?>" id="bill_no" readonly></td> 
-							        <td width="20%" class="text-center"><b>Bill Date</b></td>
-									<td width="30%"><input type="date" name="bill_date" id="bill_date" value="<?php echo date("Y-m-d"); ?>"></td> 
-						        </tr>
-								<tr>
-							        <td><b>Customer Name</b></td>
-									<td><input name="cust_name" id="cust_name"></td>
-							        <td class="text-center"><b>Customer Address</b></td>
-									<td rowspan="2"><textarea rows="4" class="form-control" name="coustomerAddress" id="Caddress" placeholder="Enter coustomer Address"style="border:1px solid black;margin-top:5px;"></textarea></td> 
-						        </tr>
-								<tr>
-							        <td><b>Mobile</b></td>
-									<td><input name="Cmobile" id="Cmobile"></td>
-									<td>&nbsp;</td>
-						        </tr>
-								<tr>
-							        <td>&nbsp;</td>
-									
-									<td colspan="2" class="text-center"><span  style="color:#0e7517;font-weight:bold;" id="addcustomer"></span></td>
-									<td></td>
-						        </tr>
-						        
-					       </tbody>
-				        </table>	
-						<div class="row mt-3" id="rep" style="overflow:auto">
-							  <table class="table text-center">
-								<tr>
-									<td>
-									  <div class="form-group autocomplete">
-										<label class="text-white"><b>Product Name</b></label>
-										<input type="text" class="form-control" name="name" id="product_name" autocomplete="off">
-									  </div>
-									</td>
-									<td>
-									  <div class="form-group">
-										<label class="text-white"><b>Product Code</b></label>
-										<input type="text" class="form-control" name="product_code" autocomplete="off" id="product_code">
-									  </div>
-									</td>
-									<!--<td>
-									  <div class="form-group">
-										<label class="text-white"><b>Description</b></label>
-										<input type="text" class="form-control" name ="PDescription" id ="PDescription"\>
-									  </div>
-									</td> -->
-									<td class="text-center">
-									   <div class="form-group">
-										<label class="text-white"><b>Qty</b></label>
-										<input type="text" class="form-control" name ="Qty" id="Qty" \>
-									  </div>
-									</td>
-									<td>
-									  <div class="form-group">
-										<label class="text-white"><b>Unit Price</b></label>
-										<input type="text" class="form-control" name ="Price" id ="Price"\>
-										<input type="hidden" name="CATID" id="CAT_ID">
-									  </div>
-									</td>
-									<td>
-									  <div class="form-group">
-										<label class="text-white"><b>Discount Amt</b></label>
-										<input type="text" class="form-control" name ="PDIS"  id ="PDIS"\>
-									  </div>
-									</td>
-									<td>
-									  <div class="form-group">
-										<label class="text-white"><b>Total Price</b></label>
-										<input type="text" class="form-control" name ="TotalRS" id ="TotalRS"\>
-									  </div>
-									</td>
-									<td width="15%" style="vertical-align:middle">
-									   <a class="btn btn-success" id="ADDORDER" ><i class="fa fa-plus" style="font-size: 14px;padding: 0px;width: 9px;"></i></a>
-									   <a class="btn btn-danger" id="REMOVEORDER"><i class="fa fa-minus" style="font-size: 14px;padding: 0px;width: 9px;"></i></a>
-								    </td>
-								</tr>
-								<input type="hidden" name="ccode" id="C_CODE" >
-							</table>
-						    
-                          <table class="table text-center list" id="testTable">
-							  <thead class="bg-success text-white">
-								  <tr>
-									<th>S No.</th>
-									<th>Product Code</th>
-									<th>Product Name</th>
-									<th>Qty</th>
-									<th>Unit Price</th>
-									<th>Total Price</th>
-								  </tr>
-							   </thead>
-							   <tbody id="tablebody">
-							      
-								   
-								   
-							   </tbody>
-							</table>
-						</div>
-						<table style="font-size:14px;width:100%;margin-top:20px;">
-						    <tbody>
-							    <tr>
-							        <td><b>Discount Amount</b>	</td>
-									<td><input name="totDiscount" id="totDiscount"></td>
-							        <td width="20%" class="text-center"><b>GST  %</b></td>
-									<td width="10%"><input name="GST" id="GST" value="18" readonly></td>
-							        <td width="20%" class="text-center"><b>Grand Total</b></td>
-									<td width="10%"><input name="GRAND" id="GRAND"></td>
-									<input type="hidden" name="GSTAMOUNT" id="GSTAMOUNT">
-						        </tr>
-								
-								<tr>
-								   <td colspan="6">&nbsp;</td>
-								</tr>
-						        <tr>
-								    <td colspan="2" onclick="tableToExcel('testTable', 'Student')" ><center><a class="btn btn-success" id="download" ><i class="fa fa-download"></i></a></center></td>
-									<td colspan="2" >
-										<center><input type="submit" id="bgenerate" name="submit" value="Generete Bill" style="background-color:#1a3b5e;color:#ffffff;display:block;"></center>
-											
-									</td>
-									<td colspan="2" onclick="printbill();"><center><a class="btn btn-danger" id="print" ><i class="fa fa-print"></i></a></center></td>
-						        </tr>
-					       </tbody>
-				        </table>
-			        </form>			
-		        </div>
+        <div class="bill p-3" style="background-color:lightgrey;overflow:auto">
+            <form id="myform" >
+                <table style="font-size:14px;width:100%;">
+                    <tbody>
+                        <tr>
+                            <td width="20%"><b>Bill No</b></td>
+                            <td width="30%"><input name="bill_no" value="<?php echo(rand(10,100)); ?>" id="bill_no" readonly></td> 
+                            <td width="20%" class="text-center"><b>Bill Date</b></td>
+                            <td width="30%"><input type="date" name="bill_date" id="bill_date" value="<?php echo date("Y-m-d"); ?>"></td> 
+                        </tr>
+                        <tr>
+                            <td><b>Customer Name</b></td>
+                            <td><input name="cust_name" id="cust_name"></td>
+                            <td class="text-center"><b>Customer Address</b></td>
+                            <td rowspan="2"><textarea rows="4" class="form-control" name="coustomerAddress" id="Caddress" placeholder="Enter coustomer Address"style="border:1px solid black;margin-top:5px;"></textarea></td> 
+                        </tr>
+                        <tr>
+                            <td><b>Mobile</b></td>
+                            <td><input name="Cmobile" id="Cmobile"></td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            
+                            <td colspan="2" class="text-center"><span  style="color:#0e7517;font-weight:bold;" id="addcustomer"></span></td>
+                            <td></td>
+                        </tr>
+                        
+                    </tbody>
+                </table>	
+                <div class="row mt-3" id="rep" style="overflow:auto">
+                        <table class="table text-center">
+                        <tr>
+                            <td>
+                                <div class="form-group autocomplete">
+                                <label class="text-white"><b>Product Name</b></label>
+                                <input type="text" class="form-control" name="name" id="product_name" autocomplete="off">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="form-group">
+                                <label class="text-white"><b>Product Code</b></label>
+                                <input type="text" class="form-control" name="product_code" autocomplete="off" id="product_code">
+                                </div>
+                            </td>
+                            <!--<td>
+                                <div class="form-group">
+                                <label class="text-white"><b>Description</b></label>
+                                <input type="text" class="form-control" name ="PDescription" id ="PDescription"\>
+                                </div>
+                            </td> -->
+                            <td class="text-center">
+                                <div class="form-group">
+                                <label class="text-white"><b>Qty</b></label>
+                                <input type="text" class="form-control" name ="Qty" id="Qty" \>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="form-group">
+                                <label class="text-white"><b>Unit Price</b></label>
+                                <input type="text" class="form-control" name ="Price" id ="Price"\>
+                                <input type="hidden" name="CATID" id="CAT_ID">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="form-group">
+                                <label class="text-white"><b>Discount Amt</b></label>
+                                <input type="text" class="form-control" name ="PDIS"  id ="PDIS"\>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="form-group">
+                                <label class="text-white"><b>Total Price</b></label>
+                                <input type="text" class="form-control" name ="TotalRS" id ="TotalRS"\>
+                                </div>
+                            </td>
+                            <td width="15%" style="vertical-align:middle">
+                                <a class="btn btn-success" id="ADDORDER" ><i class="fa fa-plus" style="font-size: 14px;padding: 0px;width: 9px;"></i></a>
+                                <a class="btn btn-danger" id="REMOVEORDER"><i class="fa fa-minus" style="font-size: 14px;padding: 0px;width: 9px;"></i></a>
+                            </td>
+                        </tr>
+                        <input type="hidden" name="ccode" id="C_CODE" >
+                    </table>
+                    
+                    <table class="table text-center list" id="testTable">
+                        <thead class="bg-success text-white">
+                            <tr>
+                            <th>S No.</th>
+                            <th>Product Code</th>
+                            <th>Product Name</th>
+                            <th>Qty</th>
+                            <th>Unit Price</th>
+                            <th>Total Price</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tablebody">
+                            
+                            
+                            
+                        </tbody>
+                    </table>
+                </div>
+                <table style="font-size:14px;width:100%;margin-top:20px;">
+                    <tbody>
+                        <tr>
+                            <td><b>Discount Amount</b>	</td>
+                            <td><input name="totDiscount" id="totDiscount"></td>
+                            <td width="20%" class="text-center"><b>GST  %</b></td>
+                            <td width="10%"><input name="GST" id="GST" value="18" readonly></td>
+                            <td width="20%" class="text-center"><b>Grand Total</b></td>
+                            <td width="10%"><input name="GRAND" id="GRAND"></td>
+                            <input type="hidden" name="GSTAMOUNT" id="GSTAMOUNT">
+                        </tr>
+                        
+                        <tr>
+                            <td colspan="6">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" onclick="tableToExcel('testTable', 'Student')" ><center><a class="btn btn-success" id="download" ><i class="fa fa-download"></i></a></center></td>
+                            <td colspan="2" >
+                                <center><input type="submit" id="bgenerate" name="submit" value="Generete Bill" style="background-color:#1a3b5e;color:#ffffff;display:block;"></center>
+                                    
+                            </td>
+                            <td colspan="2" onclick="printbill();"><center><a class="btn btn-danger" id="print" ><i class="fa fa-print"></i></a></center></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>			
+        </div>
 			<!-- </div>-->
-	    </div>
+	</div>
 
-@endsection
+@include('layouts/footer')
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,7 +10,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.php">
+                <a class="nav-link" href="{{ url('/main') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
